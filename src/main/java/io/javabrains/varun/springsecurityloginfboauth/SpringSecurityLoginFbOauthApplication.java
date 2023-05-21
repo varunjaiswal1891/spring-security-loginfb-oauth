cf2499f5400fccc1bf94ee8aa7d6bc0f6dbabd15
Varun Jaiswal <varjaisw@cisco.com>
2023-05-21 12:19:22 +0530

    first commit 21 may oauth login demo

---
+++ src/main/java/io/javabrains/varun/springsecurityloginfboauth/SpringSecurityLoginFbOauthApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.varun.springsecurityloginfboauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLoginFbOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLoginFbOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/varun/springsecurityloginfboauth/SpringSecurityLoginFbOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.varun.springsecurityloginfboauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLoginFbOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
